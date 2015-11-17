b1f4cee8a8c47735ecc1a3aca886fbc1454fc8d4
Vinay Kumar <sdvinay@gmail.com>
2015-11-16 20:43:11 -0800

    Scoring: add concept of soft hands (ace as 11)

--- test/scoring.js
+++ test/scoring.js
@@ -3,12 +3,14 @@ var Scoring = require('../lib/scoring');
 var Cards = require('../lib/cards');
 
 
-var testScoreHand = function(handStr, expectedScore, bj) {
+var testScoreHand = function(handStr, expectedScore, bj, soft) {
 	bj = bj || false;
+	soft = soft || false;
 	var hand = Cards.makeHand(handStr);
 	var score = Scoring.scoreHand(hand);
 	expect(score).property('score', expectedScore);
 	expect(score).property('blackjack', bj);
+	expect(score).property('soft', soft);
 }
 
 describe('scoreHand', function(){
@@ -22,31 +24,31 @@ describe('scoreHand', function(){
 		testScoreHand("3H KC TD", 23);
 	});
 	it('uses an ace as 1', function(){
-		testScoreHand("7H 8C AD", 16);
+		testScoreHand("7H 8C AD", 16, soft=false);
 	});
 	it('uses an ace as 11', function(){
-		testScoreHand("7H AD", 18);
+		testScoreHand("7H AD", 18, bj=false, soft=true);
 	});
 	it('scores AA as 12 (eg, use one as 11, one as 1)', function(){
-		testScoreHand("AH AD", 12);
+		testScoreHand("AH AD", 12, bj=false, soft=true);
 	});
 	it('scores 46AA as 12 (eg, use both as 1)', function(){
-		testScoreHand("4C 6S AH AD", 12);
+		testScoreHand("4C 6S AH AD", 12, bj=false, soft=false);
 	});
 	it('credits blackjack on KA', function(){
-		testScoreHand("KH AD", 21, bj=true);
+		testScoreHand("KH AD", 21, bj=true, soft=true);
 	});
 	it('credits blackjack on TA', function(){
-		testScoreHand("TH AD", 21, bj=true);
+		testScoreHand("TH AD", 21, bj=true, soft=true);
 	});
 	it('credits blackjack on AT', function(){
-		testScoreHand("AD TH", 21, bj=true);
+		testScoreHand("AD TH", 21, bj=true, soft=true);
 	});
 	it('credits 21 but no blackjack on A64', function(){
-		testScoreHand("AS 6C 4H", 21);
+		testScoreHand("AS 6C 4H", 21, bj=false, soft=true);
 	});
 	it('credits 21 but no blackjack on A4K6', function(){
-		testScoreHand("AC 4H KD 6D", 21);
+		testScoreHand("AC 4H KD 6D", 21, bj=false, soft=false);
 	});
 });
 
