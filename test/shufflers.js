6ee1c173d7b5eb2414a156169acbba5e893fc297
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-11-17 15:59:53 -0800

    Basic scaffolding for the pure random shuffler

---
+++ test/shufflers.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+var Cards = require('../lib/cards');
+var deckTest = require('./deck');
+var shuffler = require('../lib/shufflers/pure_random');
+
+describe('pure_random shuffler', function(){
+		deckTest.runDeckTests(Cards.makeDeck());
+});
+
+
