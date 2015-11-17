8481d6bb5067b362de9789ed58a563e23c8dcc63
Vinay Kumar <sdvinay@gmail.com>
2015-11-16 17:06:50 -0800

    More tests for cards

--- test/cards.js
+++ test/cards.js
@@ -55,3 +55,65 @@ describe('getValues and ranks', function() {
 		expect(c.getValue()).to.equal(10);
 	});
 });
+
+describe('makeHand', function() {
+	it ('handles a single card', function() {
+		var str = "7H";
+		var h = Cards.makeHand(str);
+		expect(h.length).to.equal(1);
+		expect(h[0].getName()).to.equal(str);
+		expect(h[0].getValue()).to.equal(7);
+	});
+	it ('handles an ace', function() {
+		var str = "AH";
+		var h = Cards.makeHand(str);
+		expect(h.length).to.equal(1);
+		expect(h[0].getName()).to.equal(str);
+		expect(h[0].getValue()).to.equal(1);
+	});
+	it ('handles a king', function() {
+		var str = "KH";
+		var h = Cards.makeHand(str);
+		expect(h.length).to.equal(1);
+		expect(h[0].getName()).to.equal(str);
+		expect(h[0].getValue()).to.equal(10);
+		expect(h[0].rank).to.equal(13);
+	});
+	it ('handles two cards', function() {
+		var str = "7H AC";
+		var h = Cards.makeHand(str);
+		expect(h.length).to.equal(2);
+		expect(h[0].getName()).to.equal(str.split(' ')[0]);
+		expect(h[1].getName()).to.equal(str.split(' ')[1]);
+		expect(h[0].getValue()).to.equal(7);
+		expect(h[1].getValue()).to.equal(1);
+	});
+	it ('handles three cards', function() {
+		var str = "7H AC TD";
+		var h = Cards.makeHand(str);
+		expect(h.length).to.equal(3);
+		expect(h.map(x => x.getName()).join(" ")).to.equal(str);
+		expect(h[0].getValue()).to.equal(7);
+		expect(h[1].getValue()).to.equal(1);
+		expect(h[2].getValue()).to.equal(10);
+	});
+	it ('throws on a bad rank', function() {
+		var str = "FH";
+		var fn = function() { Cards.makeHand(str);};
+		expect(fn).to.throw(Error);
+	});
+});
+
+
+describe('makeHand and displayHand correspond', function() {
+	it ('for a single card', function() {
+		var str = "7H";
+		var h = Cards.makeHand(str);
+		expect(Cards.displayHand(h)).to.equal(str);
+	});
+	it ('for three cards', function() {
+		var str = "7H AC TD";
+		var h = Cards.makeHand(str);
+		expect(Cards.displayHand(h)).to.equal(str);
+	});
+});
