598fe4126518e399fc270531ed8e225e4fa62eb0
Vinay Kumar <sdvinay@gmail.com>
2015-11-16 16:30:09 -0800

    Added tests for basic Cards functionality

--- test/cards.js
+++ test/cards.js
@@ -16,3 +16,42 @@ describe('makeDeck', function(){
 		}
 	});
 });
+
+describe('getName and ranks', function() {
+	it ('handles ace', function() {
+		var c = new Cards.Card(1, "H");
+		expect(c.getName()).to.equal("AH");
+	});
+	it ('handles numbers', function() {
+		var c = new Cards.Card(2, "H");
+		expect(c.getName()).to.equal("2H");
+	});
+	it ('handles ten', function() {
+		var c = new Cards.Card(10, "H");
+		expect(c.getName()).to.equal("TH");
+	});
+	it ('handles king', function() {
+		var c = new Cards.Card(13, "H");
+		expect(c.getName()).to.equal("KH");
+	});
+});
+
+
+describe('getValues and ranks', function() {
+	it ('handles ace', function() {
+		var c = new Cards.Card(1, "H");
+		expect(c.getValue()).to.equal(1);
+	});
+	it ('handles numbers', function() {
+		var c = new Cards.Card(2, "H");
+		expect(c.getValue()).to.equal(2);
+	});
+	it ('handles ten', function() {
+		var c = new Cards.Card(10, "H");
+		expect(c.getValue()).to.equal(10);
+	});
+	it ('handles king', function() {
+		var c = new Cards.Card(13, "H");
+		expect(c.getValue()).to.equal(10);
+	});
+});
