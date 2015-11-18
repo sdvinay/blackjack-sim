ca2fd4678dff87748859380a4b143bc65932a684
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-11-17 16:04:24 -0800

    Actually call the shuffler in the test

--- test/shufflers.js
+++ test/shufflers.js
@@ -4,7 +4,7 @@ var deckTest = require('./deck');
 var shuffler = require('../lib/shufflers/pure_random');
 
 describe('pure_random shuffler', function(){
-		deckTest.runDeckTests(Cards.makeDeck());
+		deckTest.runDeckTests(shuffler.shuffle(Cards.makeDeck()));
 });
 
 
