9d332453a8ffe616c0486fba5ddac23eaa3cd269
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-11-18 10:42:28 -0800

    Added tests for dealer strategy

---
+++ test/strategies/dealer.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var Cards = require('../../lib/cards');
+var strategy = require('../../lib/strategies/dealer');
+
+// This test function is dealer-specific, as there's no way to specify
+// the dealer hole card.
+function testHitDecision(handStr, exp) {
+	var desc = exp + "s on " + handStr;
+	it (desc, function() {
+		var h = Cards.makeHand(handStr);
+		var decision = strategy.hitDecision(h, h[0]);
+		expect(decision).to.equal(exp);
+	});
+}
+describe('dealerStrategy', function() {
+	describe('hitDecision', function() {
+		testHitDecision("6S 4C", "hit");
+		testHitDecision("TS 4C", "hit");
+		testHitDecision("TS 7C", "stand");
+	});
+});
