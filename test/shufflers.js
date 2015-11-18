71d011ff94c98366633ab56e7f0485d015cbd186
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-11-17 16:19:12 -0800

    Test to require that shuffled deck varies from original

--- test/shufflers.js
+++ test/shufflers.js
@@ -4,7 +4,12 @@ var deckTest = require('./deck');
 var shuffler = require('../lib/shufflers/pure_random');
 
 describe('pure_random shuffler', function(){
-		deckTest.runDeckTests(shuffler.shuffle(Cards.makeDeck()));
+	deckTest.runDeckTests(shuffler.shuffle(Cards.makeDeck()));
+
+	it('returns a different deck than passed in', function() {
+		var deck = Cards.makeDeck();
+		expect(shuffler.shuffle(deck.slice())).to.not.deep.equal(deck);
+	});
 });
 
 
