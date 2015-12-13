74ffe3358a714d6733d3a52d7097a5e30c5f2abf
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2015-12-12 16:20:06 -0800

    Basic strategy working

--- test/strategies/basic.js
+++ test/strategies/basic.js
@@ -1,6 +1,6 @@
 var expect = require('chai').expect;
 var Cards = require('../../lib/cards');
-var strategy = require('../../lib/strategies/dealer');
+var strategy = require('../../lib/strategies/basic');
 
 // This test function is dealer-specific, as there's no way to specify
 // the dealer hole card.
@@ -13,12 +13,34 @@ function testHitDecision(handStr, holeCard, exp) {
 		expect(decision).to.equal(exp);
 	});
 }
-describe('dealerStrategy', function() {
+describe('basicStrategy', function() {
 	describe('hitDecision', function() {
+		testHitDecision("6S 2C", "JC", "hit");
+		testHitDecision("6S 2C", "7C", "hit");
+		testHitDecision("6S 3C", "7C", "hit");
 		testHitDecision("6S 4C", "JC", "hit");
+		testHitDecision("6S 4C", "6C", "hit");
+		testHitDecision("6S 5C", "6C", "hit");
+		testHitDecision("6S 5C", "JC", "hit");
+		testHitDecision("6S 5C", "AC", "hit");
+		testHitDecision("7S 4C", "AC", "hit");
+		testHitDecision("8S 4C", "AC", "hit");
+		testHitDecision("8S 4C", "2C", "hit");
+		testHitDecision("8S 4C", "3C", "hit");
+		testHitDecision("8S 4C", "4C", "stand");
+		testHitDecision("8S 4C", "6C", "stand");
+		testHitDecision("8S 4C", "7C", "hit");
+		testHitDecision("9S 4C", "2C", "stand");
+		testHitDecision("9S 4C", "6C", "stand");
+		testHitDecision("9S 4C", "7C", "hit");
 		testHitDecision("TS 4C", "JC", "hit");
+		testHitDecision("TS 4C", "AC", "hit");
+		testHitDecision("TS 4C", "6S", "stand");
+		testHitDecision("TS 5C", "AS", "hit");
 		testHitDecision("TS 7C", "JC", "stand");
-		testHitDecision("AS 7C", "JC", "stand");
+		testHitDecision("TS 7C", "AC", "stand");
+		testHitDecision("TS 8C", "JC", "stand");
+		testHitDecision("AS 7C", "JC", "hit");
 		testHitDecision("AS 6C", "JC", "hit");
 		testHitDecision("AS 6C TD", "JC", "stand");
 	});
